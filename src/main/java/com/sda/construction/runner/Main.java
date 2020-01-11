12f6a43d2388bc8b964b90ae43565ff20b436602
Andrei Jitaru <andreijitaru9@gmail.com>
2020-01-11 11:12:37 +0200

    -add pom to repo

--- src/main/java/com/sda/construction/runner/Main.java
+++ src/main/java/com/sda/construction/runner/Main.java
@@ -1,12 +1,7 @@
 package com.sda.construction.runner;
 
-import com.sda.construction.config.DIConfig;
-import com.sda.construction.dto.CaramidaDTO;
-import com.sda.construction.service.CaramidaService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
